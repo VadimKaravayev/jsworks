e3f94e4d13ba36e7fba950fc1b0eb03886c67cf4
vadimkaravayev <vadim.karavayev@gmail.com>
2018-09-08 18:28:17 +0300

    MVC was set up

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,2 +1,5 @@
-import x from './test';
-console.log(`I am imported x = ${x} from another module`);
+console.log('I am ok. I am the controller.');
+import str from './models/Search';
+import * as view from './views/searchView';
+console.log(`Data from Search.js: ${str}, 4 + 8 = ${view.add(4, 8)}, 5 * 23 = ${view.multiply(5, 23)}`);
+console.log(`My id: ${view.id}`);
