a4f843b287680253384e99d14fa1da4fa79e3e8a
vadimkaravayev <vadim.karavayev@gmail.com>
2018-09-11 00:31:20 +0300

    completed video 141

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,5 +1,7 @@
 console.log('I am ok. I am the controller.');
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
 /** Global state of the app
 * - Search object
@@ -10,22 +12,26 @@ import Search from './models/Search';
 const state = {};
 const controlSearch = async () => {
   //1. Get query from view
-  const query = 'pizza';
+  const query = searchView.getInput();
+  console.log(query);
   if (query) {
     //2. New search object and add to state
     state.search = new Search(query);
 
     //3. Prepare results for UI
+    searchView.clearInput();
+    searchView.clearResults();
 
     //4. Search for recipes
     await state.search.getResults();
 
     //5. Render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result)
+    //console.log(state.search.result);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
