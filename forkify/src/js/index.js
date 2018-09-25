1e152bf0ef33f38450240f540f7083704b7d9d58
vadimkaravayev <vadim.karavayev@gmail.com>
2018-09-25 23:45:02 +0300

    completed video 155

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,9 +1,13 @@
 console.log('I am ok. I am the controller.');
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
+
 
 /** Global state of the app
 * - Search object
@@ -56,7 +60,6 @@ elements.searchResPages.addEventListener('click', (e) => {
     const goToPage = parseInt(btn.dataset.goto, 10);
     searchView.clearResults();
     searchView.renderResults(state.search.result, goToPage);
-    console.log(goToPage);
   }
 });
 
@@ -67,7 +70,6 @@ elements.searchResPages.addEventListener('click', (e) => {
 
 const controlRecipe = async () => {
   const id = window.location.hash.replace('#', '');
-  console.log(id);
   if (id) {
     //Prepare UI for changes
     recipeView.clearRecipe();
@@ -86,11 +88,7 @@ const controlRecipe = async () => {
 
       //Render recipe
       clearLoader();
-      console.log(state.recipe);
-      console.log("Hui");
       recipeView.renderRecipe(state.recipe);
-      console.log("Hui tam");
-      console.log(state.recipe);
 
     } catch(error) {
       console.log(error);
@@ -104,6 +102,51 @@ const controlRecipe = async () => {
 //window.addEventListener('hashchange', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+/**
+ * LIST CONTROLLER
+*/
+
+const controlList = () => {
+  if (!state.list) state.list = new List();
+
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+/**
+ * LIKE CONTROLLER
+*/
+
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id; 
+  if (!state.likes.isLiked(currentID)) {
+    const newLike = state.likes.addLike(
+      currentID, 
+      state.recipe.title, 
+      state.recipe.author, 
+      state.recipe.img);
+    console.log(state.likes);
+  } else {
+    state.likes.deleteLike(currentID);
+    console.log(state.likes);
+  }
+};
+
+// handle delete and update list item event
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    state.list.deleteItem(id);
+    listView.deleteItem(id);
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 elements.recipe.addEventListener('click', e => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
     if (state.recipe.servings > 1) {
@@ -114,6 +157,10 @@ elements.recipe.addEventListener('click', e => {
   } else if (e.target.matches('.btn-increase, .btn-increase *')) {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    controlLike();
   }
-  console.log(state.recipe);
 });
+
